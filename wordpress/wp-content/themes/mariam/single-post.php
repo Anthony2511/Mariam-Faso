62009623419d0c4d5a9cdaaa3b46085be0e6976e
Anthony2511 <anthonybeaumecker@gmail.com>
2017-06-16 18:24:49 +0200

    Added page css & WP single-actu

--- wordpress/wp-content/themes/mariam/single-post.php
+++ wordpress/wp-content/themes/mariam/single-post.php
@@ -12,18 +12,26 @@ Template Name: Single Post
   <?php get_header(); ?>
 
   <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
-  <div class="voyage-post">
+  <div class="single-actu">
+    <div class="single-actu__bloc-one">
+      <?php $actuImages = get_field('event__img'); ?>
+      <figure class="news__bloc-img">
+        <?php if( !empty($actuImages) ): ?>
+          <?php $size = 'thumb-actu';
+                $thumb = $actuImages['sizes'][ $size ]; ?>
+          <img class="single-actu__img" src="<?= $thumb; ?>" width="495" height="371" alt="<?= $actuImages['alt']; ?>"/>
+        <?php endif; ?>
+      </figure>
+      <p class="single-actu__text"><?= get_field('actu__text-one'); ?></p>
+    </div>
 
-  <?= 'salut'; ?>
-  <?php $actuImages = get_field('event__img'); ?>
-  <figure class="news__bloc-img">
-    <?php if( !empty($actuImages) ): ?>
-      <?php $size = 'thumb-events';
-            $thumb = $actuImages['sizes'][ $size ]; ?>
-      <img class="news__img" src="<?= $thumb; ?>" width="440" height="330" alt="<?= $actuImages['alt']; ?>"/>
-    <?php endif; ?>
-  </figure>
-
+    <?php if( have_rows('paragraphe') ): ?>
+    <?php while( have_rows('paragraphe') ): the_row(); ?>
+    <div class="single-actu__bloc-two">
+      <h3 class="single-actu__title"><?= the_sub_field('actu__text-two-title'); ?></h3>
+      <?= the_sub_field('actu__text-two'); ?>
+    </div>
+    <?php endwhile; endif; ?>
   </div>
   <?php endwhile; endif; ?>
-  <?php get_footer(); ?>
+  <?php include('footer.php'); ?>
